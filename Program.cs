88e48b7ccfc17f5a5b280df6ed607ae832baa07f
Luis Felipe <luisenrique.felipegutierrez@gmail.com>
2023-11-02 11:12:43 -0400

    Actualizacion

--- Program.cs
+++ Program.cs
@@ -1,28 +1,41 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NToastNotify;
+using Portafolio.Data;
+using Portafolio.Models;
 
-namespace Portafolio
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetSection("AppSettings").GetSection("Dataconexion").Value));
+builder.Services.AddMvc();
+builder.Services.AddMvc().AddNToastNotifyNoty(new NotyOptions
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-         
-        }
+    ProgressBar = true,
+    Timeout = 3000,
+    Theme = "mint"
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<Contactaccion>();
+builder.Services.AddScoped<EnviarMail>();
+builder.Services.AddCors();
 
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+app.UseNToastNotify();
+app.UseRouting();
+app.UseStaticFiles();
+app.UseCors();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(name: "Default", "{controller=Home}/{action=Index}/{id?}");
+});
+
+
+app.Run();
